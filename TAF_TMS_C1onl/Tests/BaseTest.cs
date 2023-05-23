92ec511bb8a92588707b1e9a79a7f484867a9b76
admin <nastyabonybelay@gmail.com>
2023-05-23 22:14:25 +0300

    add first commit

--- TAF_TMS_C1onl/Tests/BaseTest.cs
+++ TAF_TMS_C1onl/Tests/BaseTest.cs
@@ -1,29 +1,57 @@
+using Allure.Commons;
+using NLog;
+using NUnit.Allure.Core;
 using OpenQA.Selenium;
 using TAF_TMS_C1onl.Core;
 using TAF_TMS_C1onl.Steps;
 
 namespace TAF_TMS_C1onl.Tests;
 
+[AllureNUnit]
 [Parallelizable(ParallelScope.All)]
 public class BaseTest
 {
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
     protected IWebDriver Driver;
     protected NavigationSteps NavigationSteps;
     protected ProjectSteps ProjectSteps;
-    
+    private AllureLifecycle _allure;
+
     [SetUp]
     public void Setup()
     {
+        _logger.Trace(message: "Message level Trace");
+        _logger.Debug(message: "Message level Debug");
+        _logger.Info(message: "Message level Info");
+        _logger.Warn(message: "Message level Warn");
+        _logger.Error(message: "Message level Error");
+        _logger.Fatal(message: "Message level Fatal");
+
         Driver = new Browser().Driver;
         
         // Инициализация Steps
         NavigationSteps = new NavigationSteps(Driver);
         ProjectSteps = new ProjectSteps(Driver);
+
+        //Инициализация Allure
+        _allure = AllureLifecycle.Instance;
     }
 
     [TearDown]
     public void TearDown()
     {
+        //Проверка, что тест упал
+        if(TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+        {
+            //Создание скриншота
+            Screenshot screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+            byte[] screenshotBytes = screenshot.AsByteArray;
+
+            //Прикрепление скриншота
+            _allure.AddAttachment(name: "Screenshot", type:"image/png", screenshotBytes);
+        }
+
         Driver.Quit();
         Driver.Dispose();
     }
--- TAF_TMS_C1onl/Tests/LoginTest.cs
+++ TAF_TMS_C1onl/Tests/LoginTest.cs
@@ -1,11 +1,21 @@
+using Allure.Commons;
+using NUnit.Allure.Attributes;
 using TAF_TMS_C1onl.Utilites.Configuration;
 
 namespace TAF_TMS_C1onl.Tests;
 
 public class LoginTest : BaseTest
 {
-    [Test]
-    [SmokeTest]
+    [Test(Description = "Successful Login")]
+    [AllureSeverity(SeverityLevel.critical)]
+    [AllureOwner("User")]
+    [AllureSuite("PassedSuit")]
+    [AllureSubSuite("GUI")]
+    [AllureIssue(name:"TMS-12")]
+    [AllureTms(name:"TMS-13")]
+    [AllureTag("Smoke")]
+    [AllureLink(url:"https://onliner.by")]
+    [Description("More details description")]
     public void SuccessLoginTest()
     {
         NavigationSteps.NavigateToLoginPage();
