9f5e89fb83c110ef8f510823200987eecee41a55
admin <nastyabonybelay@gmail.com>
2023-05-24 10:50:37 +0300

    add lessons

---
+++ TAF_TMS_C1onl/Tests/LoginTest_.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Allure.Commons;
+using NUnit.Allure.Attributes;
+using TAF_TMS_C1onl.Utilites.Configuration;
+
+namespace TAF_TMS_C1onl.Tests
+{
+    public class LoginTest_ : BaseTest
+    {
+        [Test(Description = "Successful Login")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("User")]
+        [AllureSuite("PassedSuit")]
+        [AllureSubSuite("GUI")]
+        [AllureIssue("TMS-12")]
+        [AllureTms("TMS-13")]
+        [AllureTag("Smoke")]
+        [AllureLink("https://onliner.by")]
+        [Description("Более детализированное описание теста")]
+        [SmokeTest]
+        public void SuccessLoginTest()
+        {
+            NavigationSteps.NavigateToLoginPage();
+            NavigationSteps.SuccessfulLogin(Configurator.Admin);
+
+            Assert.IsTrue(NavigationSteps.DashboardPage.IsPageOpened());
+        }
+    }
+}
+
+
